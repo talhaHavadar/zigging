c7e1c814cd6b7c252fb8cda821601d35996b41b3
Talha Can Havadar <havadartalha@gmail.com>
2026-01-07 21:57:23 +0100

    feat: add raylib hello world example to main (#2)

--- build.zig
+++ build.zig
@@ -15,6 +15,19 @@ pub fn build(b: *std.Build) void {
         .name = "zigging",
     });
 
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+
     b.installArtifact(exe);
 
     {
