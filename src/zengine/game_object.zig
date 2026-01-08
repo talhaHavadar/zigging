a7e895f57dbd01b610f0716c5e939ba39385aa45
Talha Can Havadar <havadartalha@gmail.com>
2026-01-08 10:42:42 +0100

    zengine: add deinit for GameObject

--- src/zengine/game_object.zig
+++ src/zengine/game_object.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const log = std.log.scoped(.zengine_gameObject);
+const Allocator = std.mem.Allocator;
+
 const GameObject = @This();
 
 id: u64,
@@ -9,6 +11,10 @@ vtable: *const VTable,
 pub const VTable = struct {
     update: *const fn (game_object: *GameObject, delta_time: f32) anyerror!void = defaultUpdate,
 };
+
+pub fn deinit(self: GameObject, allocator: Allocator) void {
+    allocator.free(self.name);
+}
 
 pub fn defaultUpdate(self: *GameObject, delta_time: f32) anyerror!void {
     log.debug(
