43f57897ed01d90282296a34e3e7d90e30e63922
Talha Can Havadar <havadartalha@gmail.com>
2026-01-08 10:36:49 +0100

    zengine: add VTable to GameObject

--- src/zengine/game_object.zig
+++ src/zengine/game_object.zig
@@ -1,3 +1,22 @@
+const std = @import("std");
+const log = std.log.scoped(.zengine_gameObject);
 const GameObject = @This();
 
+id: u64,
 name: ?[]const u8 = null,
+vtable: *const VTable,
+
+pub const VTable = struct {
+    update: *const fn (game_object: *GameObject, delta_time: f32) anyerror!void = defaultUpdate,
+};
+
+pub fn defaultUpdate(self: *GameObject, delta_time: f32) anyerror!void {
+    log.debug(
+        "defaultUpdate(delta_time:{}) for {d}[name:{?s}] called, doing nothing..",
+        .{ delta_time, self.id, self.name },
+    );
+}
+
+pub fn update(self: *GameObject, delta_time: f32) anyerror!void {
+    try self.vtable.update(self, delta_time);
+}
