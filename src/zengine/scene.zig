41cb7d46ef18aef36738db751a052eb5af2a87d7
Talha Can Havadar <havadartalha@gmail.com>
2026-01-08 10:43:25 +0100

    zengine: make scene a game object

--- src/zengine/scene.zig
+++ src/zengine/scene.zig
@@ -2,4 +2,9 @@ const Scene = @This();
 
 game_object: GameObject,
 
+pub fn init(id: u64, name: []const u8) Scene {
+    return .{ .game_object = .{ .id = id, .name = name, .vtable = &vtable } };
+}
+const vtable: GameObject.VTable = .{};
+
 const GameObject = @import("game_object.zig");
